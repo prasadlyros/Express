ab0141a8004f4ebbb862607afcc0ffd1abefcd24
prasadlyros <prasad@lyrostech.com>
2024-09-24 15:49:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+app.get("/login", (req,res) => {
+    res.end("hello from express")
+    res.set({
+        "content-type" : "text/plain",
+        "token" : "abjhbj"
+    })
+})
+
+app.listen(3004,() => console.log('server is started'))
